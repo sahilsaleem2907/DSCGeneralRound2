b3cb4c1748901dde2ac650b662bd6fe9ed769cd6
sahics <sahi@gmail.com>
2021-01-01 23:51:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/qrcode/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity2 extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public  void run(){
+                Intent homeIntent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        },SPLASH_TIME_OUT);
+    }
+    }
